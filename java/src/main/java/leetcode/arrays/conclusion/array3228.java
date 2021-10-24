498d3f89142f4bbd47f0fc328d3f20653ee1c51c
choi hyunjin <hyunjin@munch-factory.com>
2021-10-24 19:19:43 +0900

    leetcode Update

---
+++ java/src/main/java/leetcode/arrays/conclusion/array3228.java
@@ -0,0 +1,17 @@
+package leetcode.arrays.in_pace_operation;
+
+public class array3228 {
+    public int heightChecker(int[] heights) {
+        if(heights.length == 1) return 1;
+
+        int res = 0;
+
+        int[] orderedHeights = sort(heights);
+
+        return res;
+    }
+
+    private int[] sort(int[] array){
+        
+    }
+}
