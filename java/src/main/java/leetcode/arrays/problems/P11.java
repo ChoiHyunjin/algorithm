37a92ce39b937c583ef326938a3f1d84151d327c
choi hyunjin <hyunjin@munch-factory.com>
2021-12-17 00:16:06 +0900

    11번 최다 넓이 물통 찾기 풀이

---
+++ java/src/main/java/leetcode/arrays/problems/P11.java
@@ -0,0 +1,60 @@
+package leetcode.arrays.problems;
+
+public class P11 {
+  // wrong answer
+  public int maxArea(int[] heights) {
+    int max = 0;
+    int hPtr = 0;
+    int nextHPtr = 0;
+    int tPtr = heights.length - 1;
+    int nextTPtr = heights.length - 1;
+    int temp = this.calculate(hPtr, tPtr, heights);
+    max = Math.max(max, temp);
+    while (hPtr < tPtr) {
+       while (nextHPtr < tPtr && heights[hPtr] >= heights[nextHPtr]) {
+         nextHPtr += 1;
+       }
+       while (nextTPtr > hPtr && heights[tPtr] >= heights[nextTPtr])
+         nextTPtr -= 1;
+       // if(nextHPtr >= tPtr && nextTPtr <= hPtr) break;
+       int next1 = calculate(hPtr, nextTPtr, heights);
+       int next2 = calculate(nextHPtr, tPtr, heights);
+       if (next1 < next2) {
+         hPtr = nextHPtr;
+         if(nextTPtr <= hPtr) nextTPtr = tPtr;
+         max = Math.max(max, next2);
+       } else {
+         tPtr = nextTPtr;
+         if(nextHPtr >= tPtr) nextHPtr = hPtr;
+         max = Math.max(max, next1);
+       }
+    }
+
+    return max;
+  }
+
+  // 4ms
+  public int maxArea2(int[] heights) {
+    int hPtr = 0;
+    int tPtr = heights.length - 1;
+    int max = this.calculate(hPtr, tPtr, heights);
+    while (hPtr < tPtr) {
+      if (heights[hPtr] > heights[tPtr]) tPtr--;
+      else hPtr++;
+      max = Math.max(this.calculate(hPtr, tPtr, heights), max);
+      max = Math.max(this.calculate(hPtr, tPtr, heights), max);
+    }
+
+    return max;
+  }
+
+
+  public int calculate(int start, int end, int[] heights) {
+    int y1 = heights[start];
+    int y2 = heights[end];
+    int width = end > start ? end - start : start - end;
+    int height = y1 > y2 ? y2 : y1;
+    return width * height;
+  }
+
+}
