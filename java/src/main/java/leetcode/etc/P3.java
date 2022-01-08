978474818885a0f485aa8c3698ad273b86c2b55f
choi hyunjin <hyunjin@munch-factory.com>
2022-01-08 15:53:21 +0900

    p3 문제 수정

--- java/src/main/java/leetcode/etc/P3.java
+++ java/src/main/java/leetcode/etc/P3.java
@@ -4,9 +4,6 @@ import java.util.Arrays;
 
 public class P3 {
   int lengthOfLongestSubstring(String s) {
-    if (s.length() == 0) return 0;
-    else if (s.length() == 1) return 1;
-
     int[] flags = new int[256];
     int max = 0;
     int ptr = 0;
@@ -15,7 +12,7 @@ public class P3 {
     for (int i = 0; i < s.length(); i++) {
       char ch = s.charAt(i);
       if (flags[ch] >= 0) {
-        max = max == 0 ? i : Math.max(i - ptr, max);
+        max = Math.max(i - ptr, max);
         ptr = Math.max(flags[ch]+1, ptr);
       }
       flags[ch] = i;
