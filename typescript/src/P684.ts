24c9fc86f53946cce02442ae0fca094d4e40eeb6
choi hyunjin <guswls3373@gmail.com>
2022-06-25 15:31:14 +0900

    graph 문제 1종 추가

---
+++ typescript/src/P684.ts
@@ -0,0 +1,76 @@
+function findRedundantConnection(edges: number[][]): number[] {
+    let res: number[] = []
+
+    const list: Edge[] = new Array(edges.length)
+
+    for(let edge of edges){
+        if(list[edge[0]] == null){
+            list[edge[0]] = new Edge(edge[0])
+        }
+        if(list[edge[1]] == null){
+            list[edge[1]] = new Edge(edge[1])
+        }
+        list[edge[0]].link(list[edge[1]])
+        list[edge[1]].link(list[edge[0]])
+    }
+
+    let isRemoved = true
+    while(isRemoved){
+        isRemoved = false
+        for(let i=0;i<edges.length; ){
+            const edge = edges[i]
+            let removed = false
+            if(list[edge[0]].getLinkCount() === 1){
+                const removedEdge = list[edge[0]].popEdge()
+                removedEdge?.unlink(list[edge[0]])
+                removed = true
+            }else if(list[edge[1]].getLinkCount() === 1){
+                const removedEdge = list[edge[1]].popEdge()
+                removedEdge?.unlink(list[edge[1]])
+                removed = true
+            }
+            if(!removed){
+                i+=1
+            }else {
+                edges.splice(i, 1)
+            }
+            isRemoved = isRemoved || removed
+        }
+    }
+
+    for(let i=edges.length-1 ; i>=0 ; i-=1){
+        const edge = edges[i]
+        if(list[edge[0]].getLinkCount() > 1 && list[edge[1]].getLinkCount() > 1 ){
+            return edge
+        }
+    }
+
+    return res
+}
+
+class Edge {
+    private _linked: Edge[]
+    value: number
+
+    constructor(value: number){
+        this._linked = []
+        this.value = value
+    }
+
+    link(edge: Edge){
+        this._linked.push(edge)
+    }
+
+    getLinkCount(){
+        return this._linked.length
+    }
+
+    popEdge(){
+        return this._linked.pop()
+    }
+
+    unlink(edge: Edge){
+        const idx = this._linked.indexOf(edge)
+        this._linked.splice(idx, 1)
+    }
+}
