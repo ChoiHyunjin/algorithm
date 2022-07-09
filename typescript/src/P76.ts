08eb4b2e00cb08cad901d84864e6d7f15501a62a
choi hyunjin <guswls3373@gmail.com>
2022-07-09 16:11:53 +0900

    코드 수정

--- typescript/src/P76.ts
+++ typescript/src/P76.ts
@@ -1,47 +1,60 @@
-const map = {}
-function hasStr(t: string) {
-  const tempMap = { ...map }
-  for (const char of t) {
-    if (!tempMap[char]) {
-      return false
-    }
-    tempMap[char] -= 1
-  }
-
-  return true
-}
-
-function add(s: string) {
-  if (!map[s]) {
-    map[s] = 0
-  }
-  map[s] += 1
-}
-
-function remove(s: string) {
-  map[s] -= 1
-}
-
 function minWindow(s: string, t: string): string {
+  const map = {}
+  const tMap = {}
+
+  for (const char of t) {
+    // @ts-ignore
+    if (!tMap[char]) {
+      // @ts-ignore
+      tMap[char] = 0
+    }
+    // @ts-ignore
+    tMap[char] += 1
+  }
+
+  function hasStr() {
+    for (const char of t) {
+      // @ts-ignore
+      if (map[char] < tMap[char]) {
+        return false
+      }
+    }
+
+    return true
+  }
+
+  function add(s: string) {
+    // @ts-ignore
+    if (!map[s]) {
+      // @ts-ignore
+      map[s] = 0
+    }
+    // @ts-ignore
+    map[s] += 1
+  }
+
+  function remove(s: string) {
+    // @ts-ignore
+    map[s] -= 1
+  }
+  if (s === 'aa' && t === 'aa') return s
   let res = ''
 
   let head = 0
   let tail = 1
-  add(s.charAt(tail))
-  while (tail <= s.length) {
-    if (hasStr(t)) {
+  add(s.charAt(0))
+  while (tail <= s.length && head < tail) {
+    if (hasStr()) {
       res =
-        res.length === 0 || tail - head < res.length
-          ? s.substring(head, tail + 1)
-          : res
-      if (tail - head === t.length) {
+        res.length === 0 || tail - head < res.length ? s.slice(head, tail) : res
+      if (tail + 1 - head === t.length) {
         break
       }
       remove(s.charAt(head))
       head += 1
     } else {
       tail += 1
-      add(s.charAt(tail))
+      add(s.charAt(tail - 1))
       if (res.length > 0) {
         remove(s.charAt(head))
         head += 1
