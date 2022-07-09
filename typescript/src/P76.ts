6813d52142246fd4fa0311691885e3b784e40b2d
choi hyunjin <guswls3373@gmail.com>
2022-07-09 15:16:13 +0900

    코드 수정

--- typescript/src/P76.ts
+++ typescript/src/P76.ts
@@ -1,45 +1,51 @@
 function minWindow(s: string, t: string): string {
+  const map = {}
+  function hasStr(t: string) {
+    const tempMap = { ...map }
+    for (const char of t) {
+      if (!tempMap[char]) {
+        return false
+      }
+      tempMap[char] -= 1
+    }
+
+    return true
+  }
+
+  function add(s: string) {
+    if (!map[s]) {
+      map[s] = 0
+    }
+    map[s] += 1
+  }
+
+  function remove(s: string) {
+    map[s] -= 1
+  }
+
   let res = ''
 
   let head = 0
-  let tail = t.length
+  let tail = 0
   while (tail <= s.length) {
-    const str = s.substring(head, tail)
-    const ok = hasStr(str, t)
-    if (ok) {
-      res = res.length === 0 || str.length < res.length ? str : res
-      if (str.length === t.length) {
+    if (hasStr(t)) {
+      res =
+        res.length === 0 || tail - head < res.length
+          ? s.substring(head, tail + 1)
+          : res
+      if (tail - head === t.length) {
         break
       }
+      remove(s.charAt(head))
       head += 1
     } else {
       tail += 1
+      add(s.charAt(tail))
       if (res.length > 0) {
+        remove(s.charAt(head))
         head += 1
       }
     }
-  }
-
-  return res
-}
-
-function hasStr(s: string, t: string) {
-  const map = {}
-  for (const char of s) {
-    if (!map[char]) {
-      map[char] = 0
-    }
-    map[char] += 1
-  }
-
-  let res = true
-
-  for (const char of t) {
-    if (!map[char]) {
-      res = false
-      break
-    }
-    map[char] -= 1
   }
 
   return res
