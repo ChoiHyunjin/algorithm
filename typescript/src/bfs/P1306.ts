c533d6ffdb4e5633d0626939789c73f83424dc94
Choihyunjin <guswls3373@gmail.com>
2022-08-27 14:57:35 +0900

    imp(P1306): implements bfs

--- typescript/src/bfs/P1306.ts
+++ typescript/src/bfs/P1306.ts
@@ -1,7 +1,37 @@
+// BFS
 function canReach(arr: number[], start: number): boolean {
+  const bfs: number[] = [];
+  const visited = {};
+  let result = false;
+
+  bfs.push(start);
+  while (bfs.length > 0) {
+    let idx = bfs.pop()!;
+    if (!!visited[idx]) {
+      continue;
+    }
+    visited[idx] = true;
+
+    if (arr[idx] === 0) {
+      return true;
+    }
+
+    if (idx - arr[idx] >= 0) {
+      bfs.push(idx - arr[idx]);
+    }
+    if (idx + arr[idx] < arr.length) {
+      bfs.push(idx + arr[idx]);
+    }
+  }
+
+  return result;
+}
+
+// DFS
+function canReach2(arr: number[], start: number): boolean {
   const visited = {};
 
-  function bfs(idx: number): boolean {
+  function dfs(idx: number): boolean {
     if(idx < 0 || idx >= arr.length){
       return false
     }
@@ -18,16 +48,16 @@ function canReach(arr: number[], start: number): boolean {
     const visit = visited[idx]
 
     if(!visit[0]) {
-      result = bfs(idx - arr[idx])
+      result = dfs(idx - arr[idx])
       visit[0] = true
     }
     if(!result && !visit[1]) {
-      result = bfs(idx + arr[idx])
+      result = dfs(idx + arr[idx])
       visit[1] = true
     }
 
     return result
   }
 
-  return bfs(start);
+  return dfs(start);
 }
