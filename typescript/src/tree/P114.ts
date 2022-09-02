f8220d0599399d0c0db82db5413aa1bd39c0604c
Choihyunjin <guswls3373@gmail.com>
2022-09-02 16:19:08 +0900

    imp(medium): imp 144(not solved)

---
+++ typescript/src/tree/P114.ts
@@ -0,0 +1,22 @@
+import TreeNode from "../utils/treeNode";
+
+function flatten(root: TreeNode | null): void {
+  flattenDFS(root);
+}
+
+function flattenDFS(root: TreeNode | null): TreeNode | null {
+  if (!root) {
+    return null;
+  }
+  const left = root.left;
+  const right = root.right;
+  const leftLast = !left ? left : flattenDFS(left);
+  const rightLast = (!right ? right : flattenDFS(right)) ?? root;
+  if (!leftLast) {
+    return rightLast;
+  }
+  root.right = left;
+  leftLast.right = right;
+  root.left = null;
+  return rightLast;
+}
