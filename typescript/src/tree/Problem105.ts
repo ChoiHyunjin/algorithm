e22b4fd11b73338684a237f54c19680ac2a90389
Choihyunjin <guswls3373@gmail.com>
2023-02-03 22:44:49 +0900

    imp: add tree problem

---
+++ typescript/src/tree/Problem105.ts
@@ -0,0 +1,20 @@
+import TreeNode from './TreeNode'
+function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
+  let root: TreeNode | null =
+    preorder.length > 0 ? new TreeNode(preorder[0]) : null
+
+  if (!root) {
+    return null
+  }
+  const rootIndex = inorder.indexOf(root.val)
+  root.left = buildTree(
+    preorder.slice(1, rootIndex + 1),
+    inorder.slice(0, rootIndex),
+  )
+  root.right = buildTree(
+    preorder.slice(rootIndex + 1),
+    inorder.slice(rootIndex + 1),
+  )
+
+  return root
+}
